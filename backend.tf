e558e1ebfaecbf4231fd23ebfa0a634ddbd5295a
virajitha <virajitha999@gmail.com>
2026-08-25 08:59:01 +0530

    added ansible deleted main.tf from root 25/6

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    use_lockfile = true
-    profile = "terraform"
-    bucket = "terraform-bucket-virajitha"
-    key = "vpc/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
